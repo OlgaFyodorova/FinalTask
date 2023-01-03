bd85aea3be027526158a9550c0b30deaab5723b6
Olga <helga.yudina@gmail.com>
2023-01-03 19:36:31 +0500

    scheme added

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/* Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. */
+
+string[] array1 = new string[7] {"hello", "2", "world", "computer science", "cat", "dog", "box"};
+string[] array2 = new string[array1.Length];
+
+void Result(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Result(array1, array2);
+PrintArray(array2);
